d2cd35877a44c0dba6ba791191c09dd0e39c631e
Cynthia Brown <titaniaann@gmail.com>
2016-10-24 16:49:59 -0500

    Add Files

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package PurpleHatFarm_sass
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'purplehatfarm_sass' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'purplehatfarm_sass' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'purplehatfarm_sass' ), 'purplehatfarm_sass', '<a href="http://titaniaann.com" rel="designer">Cynthia Brown</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
